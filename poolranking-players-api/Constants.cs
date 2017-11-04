f4441455eff960394d51f37dd0693ce47e2439c0
Mikko Andersson <mikko@pilssi.net>
2017-11-04 11:13:25 +0200

    DB connection info is now read from enviromental variables

--- poolranking-players-api/Constants.cs
+++ poolranking-players-api/Constants.cs
@@ -1,10 +1,12 @@
-﻿namespace poolranking_players_api
+﻿using System;
+
+namespace poolranking_players_api
 {
     public static class Constants
     {
-        public const string EndpointUri = "https://exoticwordsdb.documents.azure.com:443/";
-        public const string PrimaryKey = "yVUQ5xANU2UyoHrxwvdIJQABR43W4vcAmafBzS4JGkaWXduZIxIa9vgzwnFsrUVftBmwxJPR4NxOvJKXUihYhA==";
-        public const string DatabaseName = "Rankings";
-        public const string CollectionName = "Player";
+        public static string EndpointUri => Environment.GetEnvironmentVariable("COSMOSDB_ENDPOINT");
+        public static string PrimaryKey => Environment.GetEnvironmentVariable("COSMOSDB_KEY");
+        public static string DatabaseName => "Rankings";
+        public static string CollectionName => "Player";
     }
 }
