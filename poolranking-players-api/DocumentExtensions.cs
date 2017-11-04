c48f2a5efbbb426c5fec1ef504ba09f9b56889f1
Mikko Andersson <mikko@pilssi.net>
2017-11-04 15:24:21 +0200

    Allow classes only

--- poolranking-players-api/DocumentExtensions.cs
+++ poolranking-players-api/DocumentExtensions.cs
@@ -5,7 +5,7 @@ namespace poolranking_players_api
 {
     public static class DocumentExtensions
     {
-        public static T ConvertTo<T>(this Document document)
+        public static T ConvertTo<T>(this Document document) where T : class
         {
             return JsonConvert.DeserializeObject<T>(document.ToString());
         }
